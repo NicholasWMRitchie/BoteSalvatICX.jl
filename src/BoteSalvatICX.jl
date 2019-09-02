e7f89ed2b86d745e1a4b2e5998cbdb81def796a9
Nicholas Ritchie <NicholasWMRitchie@users.noreply.github.com>
2019-09-02 07:32:52 -0400

    Added the initial implementation files.

---
+++ src/BoteSalvatICX.jl
@@ -0,0 +1,8 @@
+module BoteSalvatICX
+
+include("xione.jl")
+export BoteSalvatElectron # The implementation data
+export boteSalvatICX # Computes the ionization crosssection for electron impact on free atoms
+export isavailable # Is the specified shell available for the specified element
+
+end # module
