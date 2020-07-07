e86e6d49f65e49ad5d5c4c6dfee6a9be8e667cae
Nicholas W. M. Ritchie <nicholas.ritchie@nist.gov>
2020-07-07 11:21:14 -0400

    Trying to build and deploy docs to nist-pages

--- docs/make.jl
+++ docs/make.jl
@@ -4,6 +4,26 @@ using BoteSalvatICX
 using Cairo
 using Fontconfig
 
+function addNISTHeaders(htmlfile::String)
+    # read HTML
+    html = transcode(String,read(htmlfile))
+    # Find </head>
+    i = findfirst(r"</[Hh][Ee][Aa][Dd]>", html)
+    # Already added???
+    j = findfirst("nist-header-footer", html)
+    if isnothing(j) && (!isnothing(i))
+        # Insert nist-pages links right before </head>
+        res = html[1:i.start-1]*
+            "<link rel=\"stylesheet\" href=\"https://pages.nist.gov/nist-header-footer/css/nist-combined.css\">\n"*
+            "<script src=\"https://pages.nist.gov/nist-header-footer/js/jquery-1.9.0.min.js\" type=\"text/javascript\" defer=\"defer\"></script>\n"*
+            "<script src=\"https://pages.nist.gov/nist-header-footer/js/nist-header-footer.js\" type=\"text/javascript\" defer=\"defer\"></script>\n"*
+            html[i.start:end]
+        write(htmlfile, res)
+    end
+    return htmlfile
+end
+
+
 rm("build",force=true,recursive=true)
 
 weaveit(name) = weave(joinpath("src", "$name"), out_path=joinpath("src", "$(splitext(name)[1]).md"), doctype="github")
@@ -19,4 +39,11 @@ makedocs(
     )
 
 map(name->rm(joinpath("src","$(splitext(name)[1]).md")), names)
+addNISTHeaders(joinpath(@__DIR__, "build","index.html"))
+addNISTHeaders.(map(name->joinpath(@__DIR__, "build", splitext(name)[1], "index.html"), names))
+deploydocs(
+    repo   = "github.com/USNISTGOV/BoteSalvatICX.jl.git",
+    branch = "nist-pages",
+    versions = "v^",
+)
 # deploydocs(repo = "github.com/USNISTGOV/BoteSalvatICX.jl.git")
